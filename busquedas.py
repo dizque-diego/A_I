be2f690fd9d6e5a75290c745dc68ed5b5f970ef3
Diego Londoño <diegoingenieria98@gmail.com>
2026-05-23 12:26:15 -0500

    se agrega estructura inicial de algoritmos de busqueda

---
+++ busquedas.py
@@ -0,0 +1,35 @@
+# Este archivo contiene las plantillas de los algoritmos de busqueda que seran implementados e para encontrar rutas dentro del sistema de transporte
+# Se contempla el uso de:
+#   *Busqueda en anchura
+#   *Busqueda en profundidad
+#   *Busqueda A*
+
+def busqueda_anchura(grafo,origen,destino):
+
+
+
+    pass
+
+
+
+def busqueda_profundidad(grafo,origen,destino):
+
+
+
+    pass
+
+
+
+def Busqueda_Astar(grafo,origen,destino):
+
+
+
+    pass
+
+
+def calculo_costo_ruta(grafo,ruta):
+
+
+
+    pass
+
